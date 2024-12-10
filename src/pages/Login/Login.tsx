c0b9b3a854d25a30f491951ffbbf829ce554ccb8
Eric Parache <aceric715@gmail.com>
2024-12-10 02:29:46 -0500

    feat: add login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../apis";
+import "./login.scss";
+
+export const Login: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [_, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    setError("");
+    try {
+      const response = await login(username, password);
+      if (response.token) {
+        sessionStorage.setItem("token", response.token);
+        sessionStorage.setItem("username", username);
+        navigate("/home", { state: { username } });
+      }
+    } catch (error: any) {
+      setError(error.response.data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login">
+        <h2 className="login__title">Login</h2>
+        <form onSubmit={handleLogin} className="login__form">
+          <label className="login__label" htmlFor="username">
+            Username:
+          </label>
+          <input
+            className="login__input"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label className="login__label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="login__input"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button className="login__button" type="submit" disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
